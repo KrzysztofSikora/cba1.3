d8c452d25d6f6d45db7fd79205714c7fcb6ef7f3
krzysztof <listdlakrzycha@gmail.com>
2017-04-05 18:58:14 +0200

    post and blog style

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: krzysztof
- * Date: 26.02.16
- * Time: 16:35
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
  */
 
-    header("Location: /wordpress/");
-    exit;
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
